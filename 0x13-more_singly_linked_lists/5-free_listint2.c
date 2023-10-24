cbf75d2d5e943de7b7452ad1e4ade355b0f35268
YAKINI BENI <y.beni250@gmail.com>
2023-10-23 23:20:50 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a linked list and sets the head pointer to NULL.
+ * @head: Pointer to the pointer to the head of the list.
+ */
+void free_listint2(listint_t **head)
+{
+    listint_t *temp;
+
+    if (!head || !(*head))
+        return;
+
+    while (*head)
+    {
+        temp = (*head)->next;
+        free(*head);
+        *head = temp;
+    }
+
+    *head = NULL;
+}
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+0x13-More_singly linked lists
